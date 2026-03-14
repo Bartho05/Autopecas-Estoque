7082322138378e19dcea51cc4ed6c36bac3d1751
Bartholomeo Rocha <eliasbartholomeu17@gmail.com>
2026-03-14 12:16:58 -0300

    f

--- Autoparts-Inventory/src/main/java/com/bartho/autoparts/repository/PartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bartho.autoparts.repository;
-
-import com.bartho.autoparts.model.Part;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PartRepository extends JpaRepository<Part, Long> {
-
-}
