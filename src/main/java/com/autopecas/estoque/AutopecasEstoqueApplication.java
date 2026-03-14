31657a8908b1f723ea8e35a60d3fb5edd7743117
Bartho05 <eliasbartholomeu17@gmail.com>
2026-03-14 12:20:46 -0300

    Corrigindo erro de Estrutura do projeto

---
+++ src/main/java/com/autopecas/estoque/AutopecasEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.autopecas.estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutopecasEstoqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutopecasEstoqueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/autopecas/estoque/AutopecasEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autopecas.estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutopecasEstoqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
